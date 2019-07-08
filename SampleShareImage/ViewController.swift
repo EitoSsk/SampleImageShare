6bead5eb69d50d4bbb5bcb413c0f2587f11e31c1
EitoSsk <bulletman813@gmail.com>
2019-07-08 14:36:46 +0900

    画像選択・共有まで

--- SampleShareImage/ViewController.swift
+++ SampleShareImage/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import AssetsLibrary
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func pickImageFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func addImage(_ sender: Any) {
+        pickImageFromLibrary()
+    }
+    
+    @IBAction func shareImage(_ sender: Any) {
+        guard let image = imageView.image else {
+            print("対象の画像が見つかりません")
+            return
+        }
+        
+        let data = image.pngData()
+        let url = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("tmp.png")
+        
+        do {
+            try data?.write(to: url!)
+        } catch {
+            // nop
+        }
+        
+        let controller = UIDocumentInteractionController.init(url: url!)
+        if !(controller.presentOpenInMenu(from: view.frame, in: view, animated: true)) {
+            print("ファイルに対応するアプリがありません")
+        }
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = originalImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
